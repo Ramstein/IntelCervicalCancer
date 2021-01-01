b5bcb387910e933bd874627394d69455bad525c2
Ramstein <gerialworld@gmail.com>
2021-01-01 22:49:53 +0530

    code changes made

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -1,14 +1,23 @@
 import os
+from os.path import join
 
 import matplotlib.pyplot as plt
 
-TRAIN_DATA_DIR = '/workdir/data/train/'
-TEST_DATA_DIR = '/workdir/data/test/'
-ADD_DATA_DIR = '/workdir/data/additional/'
-BBOX_FILES = '/workdir/data/bboxes/%s_bbox.tsv'
-SAMPLE_PATH = '/workdir/data/sample_submission.csv'
-dataset_in_pickle = '/workdir/data/train_val.pickle'
-aug_output_dir = '/workdir/data/detect_data/data001_size1024'
+# SageMakerTrainingRoot_dir = "/opt/ml/code/"  # Here /code/==/IntelCervicalCancer/
+SageMakerTrainingRoot_dir = ""
+
+if SageMakerTrainingRoot_dir:
+    SageMakerRoot_dir = SageMakerTrainingRoot_dir
+else:
+    SageMakerRoot_dir = "/home/ec2-user/SageMaker/IntelCervicalCancer"
+
+TRAIN_DATA_DIR = join(SageMakerRoot_dir, 'data/train/')
+TEST_DATA_DIR = join(SageMakerRoot_dir, 'data/test/')
+ADD_DATA_DIR = join(SageMakerRoot_dir, 'data/additional/')
+BBOX_FILES = join(SageMakerRoot_dir, 'data/bboxes/%s_bbox.tsv')
+SAMPLE_PATH = join(SageMakerRoot_dir, 'data/sample_submission.csv')
+dataset_in_pickle = join(SageMakerRoot_dir, 'data/train_val.pickle')
+aug_output_dir = join(SageMakerRoot_dir, 'data/detect_data/data001_size1024')
 
 CLASSES = ['Type_1', 'Type_2', 'Type_3']
 
