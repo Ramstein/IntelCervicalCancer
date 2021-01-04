c7d0a0927cbb4f8ba4f135f786da4be8880b9065
Ramstein <gerialworld@gmail.com>
2021-01-04 09:46:12 +0530

    code changes made

--- bbox_data_preproc.py
+++ bbox_data_preproc.py
@@ -1,12 +1,10 @@
 from os.path import join
 
 from src.scripts.bbox_data_preproc import augment_save_data_df, load_bboxes_train_val_df
-from src.scripts.utils import aug_output_dir
+from src.scripts.utils import aug_output_dir, img_size
 
 if __name__ == "__main__":
-    size = (1024, 1024)
-
     train_df, val_df = load_bboxes_train_val_df()
 
-    augment_save_data_df(train_df, join(aug_output_dir, 'train'), size)
-    augment_save_data_df(val_df, join(aug_output_dir, 'val'), size)
+    augment_save_data_df(train_df, join(aug_output_dir, 'train'), img_size)
+    augment_save_data_df(val_df, join(aug_output_dir, 'val'), img_size)
