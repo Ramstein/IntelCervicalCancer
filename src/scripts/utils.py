0caba2f81daed003be2c02a218f5b5d0e4d4c1e5
Ramstein <gerialworld@gmail.com>
2021-01-04 10:14:28 +0530

    code changes made

--- src/scripts/utils.py
+++ src/scripts/utils.py
@@ -12,8 +12,9 @@ LR = 0.0001
 n_epoch = 1000
 img_size = (1024, 1024)
 
-
+# SageMakerTrainingRoot_dir = '/opt/ml'
 SageMakerTrainingRoot_dir = ""
+
 if SageMakerTrainingRoot_dir:
     SageMakerRoot_dir = SageMakerTrainingRoot_dir
     TRAIN_DATA_DIR = join(SageMakerRoot_dir, 'input/data/train/')
@@ -28,16 +29,16 @@ if SageMakerTrainingRoot_dir:
 
 else:
     SageMakerRoot_dir = "/home/ec2-user/SageMaker"
-    TRAIN_DATA_DIR = join(SageMakerRoot_dir, 'Haemorrhage_dataset/train/')
-    TEST_DATA_DIR = join(SageMakerRoot_dir, 'Haemorrhage_dataset/test/')
-    ADD_DATA_DIR = join(SageMakerRoot_dir, 'Haemorrhage_dataset/additional/')
-    BBOX_FILES = join(SageMakerRoot_dir, 'Haemorrhage_dataset/bboxes/%s_bbox.tsv')
-    SAMPLE_PATH = join(SageMakerRoot_dir, 'Haemorrhage_dataset/sample_submission.csv')
-    dataset_in_pickle = join(SageMakerRoot_dir, 'Haemorrhage_dataset/train_val.pickle')
-    aug_output_dir = join(SageMakerRoot_dir, 'Haemorrhage_dataset/detect_data/{0}'.format(dataset_name))
-    pred_aug_output_dir = join(SageMakerRoot_dir, 'Haemorrhage_dataset/pred_aug_output')
+    TRAIN_DATA_DIR = join(SageMakerRoot_dir, 'CervicalCancer_dataset/train/')
+    TEST_DATA_DIR = join(SageMakerRoot_dir, 'CervicalCancer_dataset/test/')
+    ADD_DATA_DIR = join(SageMakerRoot_dir, 'CervicalCancer_dataset/additional/')
+    BBOX_FILES = join(SageMakerRoot_dir, 'CervicalCancer_dataset/bboxes/%s_bbox.tsv')
+    SAMPLE_PATH = join(SageMakerRoot_dir, 'CervicalCancer_dataset/sample_submission.csv')
+    dataset_in_pickle = join(SageMakerRoot_dir, 'CervicalCancer_dataset/train_val.pickle')
+    aug_output_dir = join(SageMakerRoot_dir, 'CervicalCancer_dataset/detect_data/{0}'.format(dataset_name))
+    pred_aug_output_dir = join(SageMakerRoot_dir, 'CervicalCancer_dataset/pred_aug_output')
     model_save_dir = os.path.join(SageMakerRoot_dir,
-                                  'Haemorrhage_dataset/models/{0}/{1}/'.format(dataset_name, train_name))
+                                  'CervicalCancer_dataset/models/{0}/{1}/'.format(dataset_name, train_name))
 
 # data_dir = aug_output_dir
 
