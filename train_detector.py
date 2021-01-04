d06c280b40a3a8a410bda7baed4c5dc991ba44d5
Ramstein <gerialworld@gmail.com>
2021-01-04 10:30:18 +0530

    code changes made

--- train_detector.py
+++ train_detector.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
     """MODEL"""
 
     model, criterion, optimizer = get_model()
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     """TRAIN"""
     """TRAIN"""
